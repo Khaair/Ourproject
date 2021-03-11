f56665af012bdd42435ec55247e0e5b2e7504825
khaair <khair.cse.iu@gmail.com>
2021-03-12 00:07:32 +0600

    11 tarikh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Newcom from "./Newcom";
+import Header from "./Header/Header";
+import Liton from "./Liton";
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const App = () => {
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
+      <Router>
+      <Header/>
+      <Switch>
+      <Route path="/" exact component={Liton}/>
+        <Route path="/newcom" exact component={Newcom}/>
+      </Switch>
+        
+
+      </Router>
+    
   );
-}
+};
 
 export default App;
---
+++ src/Newcom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Newcom=()=> {
+    return (
+        <div>
+            <p onClick={()=>alert('i am from event')}>i am new custom </p>
+        </div>
+    )
+}
+
+export default Newcom;
