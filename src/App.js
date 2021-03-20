4f701c3fd3e245b7130f4dea78b8b0073aeee65f
khaair <khair.cse.iu@gmail.com>
2021-03-20 23:00:05 +0600

    finalized

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [initial,setInitial]=useState('rangpur')
   const [search,setSearch]=useState('');
   useEffect(async()=>{
-    const data= await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=rangpur&appid=36322319baf21d82da4b8b8bdf9388b1`)
+    const data= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${initial}&appid=36322319baf21d82da4b8b8bdf9388b1`)
     setdata(data.data)
     console.log(data)
   },[initial])
@@ -36,7 +36,7 @@ const  setcity=(e)=>{
             <div className="info">
               <img
                 className="city-icon"
-                src=""
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
                 alt="icon"
               />
               <p>{data.weather[0].main}</p>
